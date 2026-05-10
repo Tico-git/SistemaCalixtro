653a25b00124ff0e4a3673c7f20ab4523ba08eae
Tico-git <mhsc.mgp12@gmail.com>
2026-05-10 13:11:55 -0400

    Fiz o bean da tela de licenças

---
+++ src/bean/Licencas.java
@@ -0,0 +1,73 @@
+package bean;
+
+import java.util.Date;
+
+public class Licencas {
+
+    private int id;
+    private String marca;
+    private String tipo;
+    private String empresa;
+    private Date dataInicio;
+    private Date dataExpiracao;
+    private String status;
+
+    public Licencas() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataExpiracao() {
+        return dataExpiracao;
+    }
+
+    public void setDataExpiracao(Date dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
