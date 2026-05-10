9ecab847a8ce428ddc99ff980aa3fc5a067328f9
Tico-git <mhsc.mgp12@gmail.com>
2026-05-10 13:06:33 -0400

    Fiz o bean de produtos

---
+++ src/bean/Produtos.java
@@ -0,0 +1,82 @@
+package bean;
+
+import java.util.Date;
+
+public class Produtos {
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+    private Date chegadaEstoque;
+    private String descricao;
+    private String fornecedor;
+    private String categoria;
+
+    public Produtos() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Date getChegadaEstoque() {
+        return chegadaEstoque;
+    }
+
+    public void setChegadaEstoque(Date chegadaEstoque) {
+        this.chegadaEstoque = chegadaEstoque;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
